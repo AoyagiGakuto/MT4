2f357e60fb193b60830cf9c8b4474bfe45b688cf
AoyagiGakuto <k024g0082@g.neec.ac.jp>
2025-10-17 12:11:31 +0900

    ittan

--- main.cpp
+++ main.cpp
@@ -64,19 +64,19 @@ Matrix4x4 MakeRotateAxisAngle(const Vector3 axis, float angle)
 
     Matrix4x4 result {};
 
-    result.m[0][0] = c + oneMinusC * x * x;
-    result.m[0][1] = oneMinusC * x * y + s * z;
-    result.m[0][2] = oneMinusC * x * z - s * y;
+    result.m[0][0] = x * x * oneMinusC + c;
+    result.m[0][1] = x * y * oneMinusC + z * s;
+    result.m[0][2] = x * z * oneMinusC - y * s;
     result.m[0][3] = 0.0f;
 
-    result.m[1][0] = oneMinusC * y * x - s * z;
-    result.m[1][1] = c + oneMinusC * y * y;
-    result.m[1][2] = oneMinusC * y * z + s * x;
+    result.m[1][0] = x * y * oneMinusC - z * s;
+    result.m[1][1] = y * y * oneMinusC + c;
+    result.m[1][2] = y * z * oneMinusC + x * s;
     result.m[1][3] = 0.0f;
 
-    result.m[2][0] = oneMinusC * z * x + s * y;
-    result.m[2][1] = oneMinusC * z * y - s * x;
-    result.m[2][2] = c + oneMinusC * z * z;
+    result.m[2][0] = x * z * oneMinusC + y * s;
+    result.m[2][1] = y * z * oneMinusC - x * s;
+    result.m[2][2] = z * z * oneMinusC + c;
     result.m[2][3] = 0.0f;
 
     result.m[3][0] = 0.0f;
@@ -116,9 +116,9 @@ Matrix4x4 DirectionToDirection(const Vector3 from, const Vector3 to)
     return MakeRotateAxisAngle(axis, angle);
 }
 
-void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix,const char*label)
+void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix, const char* label)
 {
-    Novice::ScreenPrintf(x, y, "%s",label);
+    Novice::ScreenPrintf(x, y, "%s", label);
 
     for (int i = 0; i < 4; i++) {
         Novice::ScreenPrintf(x, y + 20 + i * 20, "%7.3f %7.3f %7.3f %7.3f",
@@ -126,7 +126,6 @@ void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix,const char*label)
             matrix.m[i][2], matrix.m[i][3]);
     }
 }
-
 
 const char kWindowTitle[] = "LE2C_01_アオヤギ_ガクト_MT4";
 
@@ -145,8 +144,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
     Vector3 to0 = -from0;
     Vector3 from1 = Normalize(Vector3 { -0.6f, 0.9f, 0.2f });
     Vector3 to1 = Normalize(Vector3 { 0.4f, 0.7f, -0.5f });
-    Matrix4x4 rotateMatrix0 = DirectionToDirection(
-    Normalize(Vector3 { 1.0f, 0.0f, 0.0f }), Normalize(Vector3 { -1.0f, 0.0f, 0.0f }));
+    Matrix4x4 rotateMatrix0 = DirectionToDirection(Normalize(Vector3 { 1.0f, 0.0f, 0.0f }), Normalize(Vector3 { -1.0f, 0.0f, 0.0f }));
     Matrix4x4 rotateMatrix1 = DirectionToDirection(from0, to0);
     Matrix4x4 rotateMatrix2 = DirectionToDirection(from1, to1);
     int kRowHeight = 20;
@@ -171,11 +169,11 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
         ///
         /// ↓描画処理ここから
         ///
-        
+
         MatrixScreenPrintf(0, 0, rotateMatrix0, "rotateMatrix0");
         MatrixScreenPrintf(0, kRowHeight * 5, rotateMatrix1, "rotateMatrix1");
         MatrixScreenPrintf(0, kRowHeight * 10, rotateMatrix2, "rotateMatrix2");
-                ///
+        ///
         /// ↑描画処理ここまで
         ///
 
